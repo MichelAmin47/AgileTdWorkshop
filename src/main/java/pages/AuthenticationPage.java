964e95c210b2fa5c24d4308da693ad76094e3304
Michel Lalmohamed <michelamin@gmail.com>
2019-10-24 14:21:54 +0200

    remove old code

--- src/main/java/pages/AuthenticationPage.java
+++ src/main/java/pages/AuthenticationPage.java
@@ -1,14 +1,9 @@
 package pages;
 
-import org.apache.http.util.Asserts;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.junit.Assert;
-import java.util.List;
 
 public class AuthenticationPage{
     private WebDriver driver;
@@ -21,16 +16,6 @@ public class AuthenticationPage{
 
     @FindBy(css = "form#login-form button[type='submit'] ")
     private WebElement loginButton;
-
-    //-----old------
-    @FindBy(css = ".form-group.form-error #email")
-    private WebElement invalidEmail;
-
-    @FindBy(css = "[for='email_create'] + [type='email']")
-    private WebElement newUserMail;
-
-    @FindBy(css = "button#SubmitLogin")
-    private WebElement createNewUserButton;
 
 
     public AuthenticationPage(WebDriver driver) {
@@ -56,32 +41,6 @@ public class AuthenticationPage{
         emailTextfield.sendKeys(email);
         passwordTextfield.sendKeys(password);
         loginButton.click();
-    }
-
-    public void setNewUserMail(String mail){
-        newUserMail.sendKeys(mail);
-        newUserMail.sendKeys(Keys.TAB);
-    }
-
-    public void clickCreateNewUser(){
-        createNewUserButton.click();
-    }
-
-    public boolean isNewUserMailValid(){
-        List<WebElement> okElements = driver.findElements(By.cssSelector("[id='create-account_form'] [class='form-group form-ok']"));
-        List<WebElement> errorElements = driver.findElements(By.cssSelector("[id='create-account_form'] [class='form-group form-error']"));
-        System.out.println(okElements.size());
-        System.out.println(errorElements.size());
-        if(okElements.size() > 0){
-            return true;
-        }
-        if (errorElements.size() >0){
-            return false;
-        }
-        else{
-            Assert.assertTrue("The new user mail field cannot be found" , false);
-            return false;
-        }
     }
 }
 
